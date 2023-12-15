6c95dca3abe450e443605cec27714050c2a1fc80
Jimmy Liu <jimmyliu20040@gmail.com>
2023-12-15 15:08:05 -0800

    added world interationcs to add or remove voxels

--- voxel_handler.py
+++ voxel_handler.py
@@ -13,8 +13,44 @@ class VoxelHandler:
         self.voxel_index = None
         self.voxel_local_pos = None
         self.voxel_world_pos = None
-        self.voxel_normal= None
+        self.voxel_normal = None
 
+        # 0: remove voxel  1: add voxel
+        self.interaction_mode = 0
+        self.new_voxel_id = 1
+
+    def add_voxel(self):
+        if self.voxel_id:
+            # check voxel id along the normal vector
+            result = self.get_voxel_id(self.voxel_world_pos + self.voxel_normal)
+
+            # is the new place empty?
+            if not result[0]:
+                _, voxel_index, _, chunk = result
+                chunk.voxels[voxel_index] = self.new_voxel_id
+                chunk.mesh.rebuild()
+
+                # wqas it an empty chunk?
+                if chunk.is_empty:
+                    chunk.is_empty = False
+
+    def remove_voxel(self):
+        if self.voxel_id:
+            self.chunk.voxels[self.voxel_index] = 0
+
+            self.chunk.mesh.rebuild()
+
+    def set_voxel(self):
+        if self.interaction_mode:
+            self.add_voxel()
+        else:
+            self.remove_voxel()
+
+    def switch_mode(self):
+        self.interaction_mode = not self.interaction_mode
+
+    def update(self):
+        self.ray_cast()
 
     def ray_cast(self):
         # start point
@@ -26,7 +62,7 @@ class VoxelHandler:
         current_voxel_pos = glm.ivec3(x1, y1, z1)
         self.voxel_id = 0
         self.voxel_normal = glm.ivec3(0)
-        step_dir = -1;
+        step_dir = -1
 
         dx = glm.sign(x2 - x1)
         delta_x = min(dx / (x2 - x1), 10000000.0) if dx != 0 else 10000000.0
@@ -37,37 +73,54 @@ class VoxelHandler:
         max_y = delta_y * (1.0 * glm.fract(y1)) if dy > 0 else delta_y * glm.fract(y1)
 
         dz = glm.sign(z2 - z1)
-        delta_z = min(dz / (z2 - z1) , 10000000.0) if dz != 0 else 10000000.0
+        delta_z = min(dz / (z2 - z1), 10000000.0) if dz != 0 else 10000000.0
         max_z = delta_z * (1.0 - glm.fract(z1)) if dz > 0 else delta_z * glm.fract(z1)
 
         while not (max_x > 1.0 and max_y > 1.0 and max_z > 1.0):
+
+            result = self.get_voxel_id(voxel_world_pos=current_voxel_pos)
+            if result[0]:
+                self.voxel_id, self.voxel_index, self.voxel_local_pos, self.chunk = result
+                self.voxel_world_pos = current_voxel_pos
+
+                if step_dir == 0:
+                    self.voxel_normal.x = -dx
+                elif step_dir == 1:
+                    self.voxel_normal.y = -dy
+                else:
+                    self.voxel_normal.z = -dz
+                return True
+
             if max_x < max_y:
                 if max_x < max_z:
                     current_voxel_pos.x += dx
                     max_x += delta_x
+                    step_dir = 0
                 else:
                     current_voxel_pos.z += dz
                     max_z += delta_z
+                    step_dir = 2
             else:
                 if max_y < max_z:
                     current_voxel_pos.y += dy
                     max_y += delta_y
+                    step_dir = 1
                 else:
                     current_voxel_pos.z += dz
                     max_z += delta_z
+                    step_dir = 2
         return False
-
 
     def get_voxel_id(self, voxel_world_pos):
         cx, cy, cz = chunk_pos = voxel_world_pos / CHUNK_SIZE
 
-        if o <= cx < WORLD_W and 0 <= cy < WORLD_H and 0 <= cz < WORLD_D:
+        if 0 <= cx < WORLD_W and 0 <= cy < WORLD_H and 0 <= cz < WORLD_D:
             chunk_index = cx + WORLD_W * cz + WORLD_AREA * cy
             chunk = self.chunks[chunk_index]
 
             lx, ly, lz = voxel_local_pos = voxel_world_pos - chunk_pos * CHUNK_SIZE
 
-            voxel_index = lx + CHUNK_SIZE * lz * CHUNK_AREA * ly
+            voxel_index = lx + CHUNK_SIZE * lz + CHUNK_AREA * ly
             voxel_id = chunk.voxels[voxel_index]
 
             return voxel_id, voxel_index, voxel_local_pos, chunk
